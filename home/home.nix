360d2dea6f5707f1d63b1fb8ff5e1c5e375aab0b
enova <enovale@proton.me>
2025-06-29 07:26:18 -0500

    move zsh

--- home/home.nix
+++ home/home.nix
@@ -39,7 +39,6 @@
       nixd
       feh
       file
-      vscodium
       signal-desktop
       cinny-desktop
       strawberry
@@ -90,37 +89,11 @@
   services.kdeconnect.indicator = true;
   services.playerctld.enable = true;
 
-  programs.zsh = {
+  programs.vscode = {
     enable = true;
-    initContent = lib.mkAfter ''source "${./p10k.zsh}"; export ZSH_HIGHLIGHT_STYLES[comment]="fg=8"'';
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.8.0";
-          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
-        };
-      }
-    ];
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "sudo"
-      ];
-    };
+    package = pkgs.vscodium;
   };
+
   home.sessionVariables = {
     COMPLETION_WAITING_DOTS = "true";
     EDITOR = "nano";
