3727663936319f70914861b5d6eacd6212ac0129
Enovale <enovale@proton.me>
2025-08-04 06:12:20 -0500

    moonlight-canary: media-fetcher-releases-unstable-2025-07-27 -> 0.1.11-unstable-2025-07-13
    Diff: https://github.com/MeguminSama/moonlight-launcher/compare/c426c3343421c8176ae740d4966f65352af77335...c426c3343421c8176ae740d4966f65352af77335

---
+++ packages/moonlight-canary/default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  pkg-config,
+  dbus,
+  fetchFromGitHub,
+  rustPlatform,
+  nix-update-script,
+  inputs,
+}:
+rustPlatform.buildRustPackage (finalAttrs: rec {
+  pname = "media-fetcher";
+  version = "0.1.11-unstable-2025-07-13";
+
+  src = fetchFromGitHub {
+    owner = "MeguminSama";
+    repo = "moonlight-launcher";
+    rev = "c426c3343421c8176ae740d4966f65352af77335";
+    sha256 = "";
+  };
+
+  passthru.updateScript = nix-update-script {
+    extraArgs = [
+      "--version"
+      "branch=HEAD"
+    ];
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    dbus
+  ];
+
+  cargoHash = "";
+
+  meta = with lib; {
+    description = "Moonlight Launcher (Canary Ver)";
+    homepage = "https://github.com/MeguminSama/moonlight-launcher";
+    license = licenses.gpl3;
+  };
+})
